5ec8175d4c7fb185a8dbd5ddbc28a387f837d526
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-12-09 15:35:25 +0100

    Commit angu

--- mundial/src/app/componentes/delete-jugadores/delete-jugadores.component.ts
+++ mundial/src/app/componentes/delete-jugadores/delete-jugadores.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JugadoresServiceService } from 'src/app/servicios/jugadores-service.service';
 
 @Component({
   selector: 'app-delete-jugadores',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class DeleteJugadoresComponent {
 
+  idPlayer:number=0;
+
+  constructor(private servicioJugadores:JugadoresServiceService){}
+
+  deletePlayer(){
+    let id = this.idPlayer;
+    this.servicioJugadores.deleteJugadores(id).subscribe();
+  }
 }
--- mundial/src/app/componentes/put-jugadores/put-jugadores.component.ts
+++ mundial/src/app/componentes/put-jugadores/put-jugadores.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Jugadores } from 'src/app/clases/jugadores';
+import { JugadoresServiceService } from 'src/app/servicios/jugadores-service.service';
 
 @Component({
   selector: 'app-put-jugadores',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class PutJugadoresComponent {
 
+  id:any;
+  name:string = '';
+  age:number =0;
+  posicion:any;
+
+  constructor(private servicioJugadores:JugadoresServiceService){}
+
+  putPlayer(){
+    let player:Jugadores = {
+      name:this.name,
+      age:this.age,
+      posicion:this.posicion,
+    };
+
+    this.servicioJugadores.putJugadores(player).subscribe();
+  }
 }
