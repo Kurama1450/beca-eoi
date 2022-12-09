1a66fbccf52b11baa6634c057f78751f2a174562
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-12-09 17:24:27 +0100

    Commit angu

--- mundial/src/app/app-routing.module.ts
+++ mundial/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DeleteJugadoresComponent } from './componentes/delete-jugadores/delete-jugadores.component';
+import { DeleteSeleccionesComponent } from './componentes/delete-selecciones/delete-selecciones.component';
 import { GetJugadoresComponent } from './componentes/get-jugadores/get-jugadores.component';
+import { GetSeleccionesComponent } from './componentes/get-selecciones/get-selecciones.component';
 import { ListaJugadoresComponent } from './componentes/lista-jugadores/lista-jugadores.component';
 import { PostJugadoresComponent } from './componentes/post-jugadores/post-jugadores.component';
+import { PostSeleccionesComponent } from './componentes/post-selecciones/post-selecciones.component';
 import { PutJugadoresComponent } from './componentes/put-jugadores/put-jugadores.component';
+import { PutSeleccionesComponent } from './componentes/put-selecciones/put-selecciones.component';
 
 const routes: Routes = [
   {
@@ -18,6 +22,18 @@ const routes: Routes = [
   },
   {
     path:'go-get-jugadores',component:GetJugadoresComponent
+  },
+  {
+    path:'go-get-selecciones',component:GetSeleccionesComponent
+  },
+  {
+    path:'go-post-selecciones',component:PostSeleccionesComponent
+  },
+  {
+    path:'go-put-selecciones',component:PutSeleccionesComponent
+  },
+  {
+    path:'go-delete-selecciones',component:DeleteSeleccionesComponent
   }
 ];
 
